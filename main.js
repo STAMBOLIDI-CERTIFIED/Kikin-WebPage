cf5398f831d796a72a8711b32d4e6fd18b7af185
STAMBOLIDI IVAN <sia.ocean.control@internet.ru>
2025-01-30 13:55:11 +0300

    Добавил в js файл Intersection API, пока в стадии разработки. Intersection API неправильно отображает появление в поле видимости элементы.

--- main.js
+++ main.js
@@ -1,10 +1,39 @@
-const burgerButton = document.querySelector('#burgerButton');
+const options = {
+  root: document.querySelector('#main-section1'), // Родительский контейнер
+  rootMargin: '0px', // Отступы вокруг root
+  threshold: 1, // Порог пересечения (50%)
+};
 
-burgerButton.addEventListener('click', function(){
-    if( document.querySelector('#navbar-list').style.marginLeft == '-1000px'){
-        document.querySelector('#navbar-list').style.marginLeft = '0px';
+const callback = (entries, observer) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      targetElement1.classList.add('show')
+      targetElement2.classList.add('show')
+      targetElement3.classList.add('show')
+      targetElement4.classList.add('show')
+      targetElement5.classList.add('show')
     }
-    else{
-        document.querySelector('#navbar-list').style.marginLeft = '-1000px';
-    }
-})
+  });
+};
+
+// Создаем наблюдатель
+const observer = new IntersectionObserver(callback, options);
+
+// Начинаем наблюдение за целевым элементом
+const targetElement1 = document.querySelector('.opacityImg1');
+const targetElement2 = document.querySelector('.opacityImg2');
+const targetElement3 = document.querySelector('.opacityImg3');
+const targetElement4 = document.querySelector('.opacityImg4');
+const targetElement5 = document.querySelector('.opacityImg5');
+
+
+if (targetElement1|| targetElement2 || targetElement3 || targetElement4 || targetElement5) {
+  observer.observe(targetElement1);
+  observer.observe(targetElement2);
+  observer.observe(targetElement3);
+  observer.observe(targetElement4);
+  observer.observe(targetElement5);
+} else {
+  console.error('Целевой элемент не найден!');
+}
